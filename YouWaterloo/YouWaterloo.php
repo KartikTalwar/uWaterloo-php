74460895e7c36ee0e42fd1163ddcf6c207962715
Kartik <hi@kartikt.com>
2012-11-01 19:34:56 +0000

    Added a simple helper

--- YouWaterloo/YouWaterloo.php
+++ YouWaterloo/YouWaterloo.php
@@ -7,7 +7,7 @@ class YouWaterloo
     const USER_AGENT  = 'uWaterloo-PHP';
     const REQ_LIMIT   = 5000;
     const API_VERSION = 'v1';
-    const API_URL     = 'http://api.uwaterloo.ca/pubic/';
+    const API_URL     = 'http://api.uwaterloo.ca/public/';
 
     public  $output;
     public  $reqUrl;
@@ -18,7 +18,17 @@ class YouWaterloo
     {
         $this->apiKey = $apiKey;
         $this->output = 'json';
-        $this->reqUrl = self::API_URL.self::API_VERSION.'/';
+        $this->reqUrl = self::API_URL . self::API_VERSION.'/?';
+    }
+
+
+    public function _buildQuery($params)
+    {
+        $params['key'] = $this->apiKey;
+        $queryParams   = http_build_query($params);
+        $fullQueryUrl  = $this->reqUrl . $queryParams;
+
+        return $fullQueryUrl;
     }
 
 }
